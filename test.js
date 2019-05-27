9740f7a58323d3a28f3f5da77447e2ef110a9570
Thaddee Tyl <thaddee.tyl@gmail.com>
2019-05-27 21:52:30 +0200

    Convert tests to async syntax
    Also, we rely on the new promise rejection detection from the standard assert library.

--- test.js
+++ test.js
@@ -3,64 +3,34 @@ var fsos = require('.');
 var fs = require('fs');
 var path = require('path');
 
-var thrownCount = 0;
-var assertPromiseNotThrown = function(e) {
-  //console.error(e);
-  assert(false, 'Error thrown:\n' + e.stack);
-  throw e;
-};
-
 // Test with new file.
 var testFile = 'test-1';
 var testDir = 'test-dir';
 try { fs.unlinkSync(testFile); } catch(_) {}
 
-fsos.set(testFile, 'hello')
-.then(function() {
-  return fsos.get(testFile);
-}).then(function(val) {
-  assert.equal(''+val, 'hello');
-}).catch(assertPromiseNotThrown)
+(async () => {
 
-// Test with existing file.
-.then(function() {
-  return fsos.set(testFile, 'hi');
-}).then(function() {
-  return fsos.get(testFile);
-}).then(function(val) {
-  assert.equal(''+val, 'hi');
-}).catch(assertPromiseNotThrown)
+  await fsos.set(testFile, 'hello');
+  assert.equal(String(await fsos.get(testFile)), 'hello');
 
-// Test without directory autocreation.
-.then(function() {
-  return fsos.set(path.join(testDir, testFile), 'hi', {noMkdir: true});
-}).catch(function(e) {
-  assert.equal(e.code, 'ENOENT');
-  thrownCount++;
-})
+  // Test with existing file.
+  await fsos.set(testFile, 'hi');
+  assert.equal(String(await fsos.get(testFile)), 'hi');
 
-// Test with directory autocreation.
-.then(function() {
-  return fsos.set(path.join(testDir, testFile), 'hi');
-}).then(function() {
-  return fsos.get(path.join(testDir, testFile));
-}).then(function(val) {
-  assert.equal(''+val, 'hi');
-}).catch(assertPromiseNotThrown)
+  // Test without directory autocreation.
+  await assert.rejects(
+    fsos.set(path.join(testDir, testFile), 'hi', {noMkdir: true}),
+    { code: 'ENOENT' });
 
-// Test deletion.
-.then(function() { return fsos.delete(testFile); })
-.then(function() { return fsos.delete(path.join(testDir, testFile)); })
-.then(function() { return fsos.deleteDir(testDir); })
-.then(function() {
-  return fsos.get(testFile);
-}).catch(function(e) {
-  assert.equal(e.code, 'ENOENT');
-  thrownCount++;
-}).then(function() {
-  assert.equal(thrownCount, 2);
-}).catch(assertPromiseNotThrown)
+  // Test with directory autocreation.
+  await fsos.set(path.join(testDir, testFile), 'hi');
+  assert.equal(String(await fsos.get(path.join(testDir, testFile))), 'hi');
 
-.then(function() {
+  // Test deletion.
+  await fsos.delete(testFile);
+  await fsos.delete(path.join(testDir, testFile));
+  await fsos.deleteDir(testDir);
+  await assert.rejects(fsos.get(testFile), { code: 'ENOENT' });
+
   console.log('done');
-}).catch(function(e) { console.error(e); });
+})().catch(e => { process.nextTick(() => { throw e; }); throw e; });
